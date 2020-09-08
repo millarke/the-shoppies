b6fbfe482f19c342c73106a2d23f6a95b0600d85
Keith Millar <keithmillar@hotmail.ca>
2020-09-08 08:59:17 -0400

    banner works, but ugly

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -6,10 +6,26 @@ export default function NominationList(props) {
   const nominations = props.nominations.map(nomination => {
     return <Nomination nominationRemove={props.nominationRemove} nomination={nomination}></Nomination>
   })
+
+  let banner;
+  if(props.nominations.length === 5) {
+    banner = <div className='banner' style={{visibility:'visible'}}>
+      <h3>You've Nominated 5 Movies!</h3>
+    </div>
+  } else {
+    banner = <div className='banner' style={{visibility:'hidden'}}>
+      <h3>You've Nominated 5 Movies!</h3>
+    </div>
+  }
   
   return (
-    <div className='nomination-container'>
-      {nominations}
+    <div>
+      <div className='nomination-container'>
+        {nominations}
+      </div>
+      <div>
+        {banner}
+      </div>
     </div>
   )
 }

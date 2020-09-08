6b1a9617580efbf7f54de4fe715620e8088ce4a6
Keith Millar <keithmillar@hotmail.ca>
2020-09-08 04:48:22 -0400

    css on the search results improved

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -33,10 +33,29 @@ export default function Movie(props) {
 
   return (
     <article className="search-result">
-      <div className="movie-info">
-        <img src={props.poster} alt='poster' />
-        <div className="movie-title">{props.title}</div>
-        <div className="movie-year">{props.year}</div>
+      {/* <div className="movie-info"> */}
+      <div>
+        <div className='movie-info' >
+          <img src={props.poster} alt='poster' />
+          <div>
+            <h1 className="movie-title light-font">{props.title}</h1>
+            <h3 className="movie-year light-font">{props.year}</h3>
+            {/* <button
+              className='nominate' 
+              onClick={() => {
+                props.nominationHandler({
+                  poster: props.poster,
+                  title: props.title,
+                  year: props.year,
+                  id: props.id
+                })
+              }}
+              // disabled={!idCheck}>
+              >
+                nominate
+            </button> */}
+          </div>
+        </div>
         {/* {activeButton} */}
         {/* {idCheck && <button
           className='nominate' 
